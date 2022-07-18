e7f882e268dc38a87d7d4f12093ec6bbfacc07a2
Tommy Vercetti <74338779+2402575933@users.noreply.github.com>
2022-07-18 15:35:01 +0000

    2022/7/18 synchronized 代码实例

---
+++ sample_code/synchronized/Method_1.java
@@ -0,0 +1,34 @@
+package com.tommy.管程.并发问题;
+
+
+public class Method_1 {
+
+    static int count = 0;
+    static Object obj = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i ++ ) {
+                synchronized (obj) {
+                    count ++ ;  // 临界区
+                }
+            }
+        }, "t1");
+        
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 1000; i ++ ) {
+                synchronized (obj) {
+                    count -- ;  // 临界区
+                }
+            }
+        }, "t2");
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println(count);
+    }
+}
